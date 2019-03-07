de687cdbb768128cab6aaa47bf3871edb10d3782
thejosan <704504886@qq.com>
2019-03-07 14:25:35 +0800

    122

--- test.go
+++ test.go
@@ -2,36 +2,34 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
 // 数据生产者
-func producer(header string, channel chan<- string) {
-	// 无限循环, 不停地生产数据
-	for {
-		// 将随机数和字符串格式化为字符串发送给通道
-		channel <- fmt.Sprintf("%s: %v", header, rand.Int31())
-		// 等待1秒
-		time.Sleep(time.Second)
-	}
-}
-
-// 数据消费者
-func customer(channel <-chan string) {
-	// 不停地获取数据
-	for {
-		// 从通道中取出数据, 此处会阻塞直到信道中返回数据
-		message := <-channel
-		// 打印数据
-		fmt.Println(message)
-	}
-}
+//func producer(header string, channel chan<- string) {
+//	// 无限循环, 不停地生产数据
+//	for {
+//		// 将随机数和字符串格式化为字符串发送给通道
+//		channel <- fmt.Sprintf("%s: %v", header, rand.Int31())
+//		// 等待1秒
+//		time.Sleep(time.Second)
+//	}
+//}
+//
+//// 数据消费者
+//func customer(channel <-chan string) {
+//	// 不停地获取数据
+//	for {
+//		// 从通道中取出数据, 此处会阻塞直到信道中返回数据
+//		message := <-channel
+//		// 打印数据
+//		fmt.Println(message)
+//	}
+//}
 func main() {
 	var a int = 100
 	var b int = 200
 	b, a = a, b
-	b, a = a, b
+
 	fmt.Println(a, b)
 
 }
